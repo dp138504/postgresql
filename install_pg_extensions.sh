8e128a056cb2b4f71a12e1d05ffc4554a5eb8cb9
Dylan Pitts <60586225+dp138504@users.noreply.github.com>
2025-05-02 19:59:58 -0400

    Update install_pg_extensions.sh

--- install_pg_extensions.sh
+++ install_pg_extensions.sh
@@ -34,14 +34,14 @@ for EXTENSION in ${EXTENSIONS}; do
         continue
     fi
 
-    if ["$EXTENSION" == "pgvecto.rs"]; then
+    if [ "$EXTENSION" == "pgvecto.rs" ]; then
         # Grab release assets
         wget --quiet https://github.com/tensorchord/pgvecto.rs/releases/download/v${PGVECTORS_VERSION}/vectors-pg${PG_MAJOR}_${PGVECTRS_VERSION}_amd64.deb
         wget --quiet https://github.com/tensorchord/pgvecto.rs/releases/download/v${PGVECTORS_VERSION}/vectors-pg${PG_MAJOR}_${PGVECTRS_VERSION}_amd64_extensions.deb
         wget --quiet https://github.com/tensorchord/pgvecto.rs/releases/download/v${PGVECTORS_VERSION}/vectors-pg${PG_MAJOR}_${PGVECTRS_VERSION}_amd64_public.deb
         wget --quiet https://github.com/tensorchord/pgvecto.rs/releases/download/v${PGVECTORS_VERSION}/vectors-pg${PG_MAJOR}_${PGVECTRS_VERSION}_amd64_vectors.deb
 
-        apg-get install --yes vectors-pg${PG_MAJOR}_${PGVECTORS_VERSION}_amd64*.deb
+        apt-get install --yes vectors-pg${PG_MAJOR}_${PGVECTORS_VERSION}_amd64*.deb
 
         continue
     fi
